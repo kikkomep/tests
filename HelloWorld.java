17930e160a96c26cb9f6fc83396bc06f7e185f84
Marco Enrico Piras <kikkomep@crs4.it>
2016-09-20 18:31:18 +0200

    HelloWorld with a parameter

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	
 	public static void main(String[] args){
-		System.out.println("Hello world!!!!");
+		System.out.println("Hello world" + (args.length==0 ? "" : ", " + args[0]) + "!!!");
 	}	
 }
